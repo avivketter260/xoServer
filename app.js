58a153194b5db0f0963d540e481a1a03200e7323
Aviv Ketter <ketter260@gmail.com>
2020-08-03 19:35:59 +0300

    03/08/2020 19:30

--- app.js
+++ app.js
@@ -2,78 +2,82 @@ let boardService = require('./data-access/boardService.js');
 let menu = require('./user-interface/menus.js');
 let gameDynamics = require('./logic/gameDynamics.js');
 const boardUI = require('./user-interface/board.js');
-// callbeck hell
-
-
-boardService.loadBoard(function (err, loadedBoard) { // It is widely accepted that first parameter of callbacks id optional error
-    let board;
-    if (err) { // if undifined
+let board;
+// // callbeck hell
+(async function () {
+    await boardService.loadBoard();
+    if (undefined) {
         board = boardService.makeEmptyBoard();
     } else {
-        menu.isLoadGame(function (err, requsetLoad) {
-            if (requsetLoad) {
-                board = loadedBoard;
-                console.log(`the file wad loaded! now we can save`);
-            } else {
-                board = boardService.makeEmptyBoard();
-                console.log(`new game`);
+        const answer = await menu.isLoadGame()
+        if (answer) {
+            const board = await boardService.loadBoard()
+            console.log(`the file wad loaded! now we can save`);
+            boardUI.print(board)
+        } else {
+            board = boardService.makeEmptyBoard();
+            console.log('NEW GAME');
+
+        }
+        // stepTurn(board, stepTurn);
+    }
+    let turn = 0;
+    let sign;
+const notWin = gameDynamics.isWin(board,sign)
+while(!notWin) {
+    sign = turn % 2 == 0 ? 'X' : 'O';
+    boardUI.print(board)
+    let userInput = await menu.userMove();
+    let x =[userInput[0]];
+    let y =[userInput[1]];
+        // const stepTurn = function (board) {
+            if (!gameDynamics.isValidMove(board, x, y)) {
+                console.log('not valid move please try agin');
+                // return stepTurn(board, stepTurn)
             }
 
 
-            stepTurn(board, stepTurn);
-        });
-    }
-});
-let turn = 0
-// put in gamedyn ---->  /// not sure how...
-const stepTurn = function (board, callback) {
-    let sign = turn % 2 == 0 ? 'X' : 'O';
-    boardUI.print(board)
-    menu.userMove(sign, function (x, y, err) {
-        if (err) console.error(err);
-
-        if (!gameDynamics.isValidMove(board, x, y)) {
-            console.log('not valid move please try agin');
-            return callback(board, stepTurn)
+            board[y][x] = sign;
+            turn++;
         }
+            if (gameDynamics.isWin(board, sign)) {
+                console.log(`GAME OVER.`)
+                console.log(` ${sign} symbol win!`)
+                boardUI.print(board);
+                board = boardService.makeEmptyBoard();
 
-        board[y][x] = sign;
-        turn++;
 
-
-        // put in gameDyn insted win
-
-        if (gameDynamics.isWin(board, sign)) {
-            console.log(`GAME OVER.`)
-            console.log(` ${sign} symbol win!`)
-            boardUI.print(board);
-            board = boardService.makeEmptyBoard()
-            
-            boardService.saveBoard(board, function (err) {
-                if (err) return console.log(err); // if undefined
+                boardService.saveBoard(board);
                 console.log('cleaning board ... ')
-            });
 
-            menu.isRestart(function (err, restart) {
+                const restart = await menu.isRestart()
                 if (restart) {
-                    callback(board, stepTurn);
+                    
                 }
-            })
-        } else {
 
-            boardService.saveBoard(board, function (err) {
-                if (err) return console.log(err); // if undefined
+            } else {
 
+               await boardService.saveBoard(board);
                 console.log("The file was saved! now we can load");
-                callback(board, stepTurn);
-            });
-        }
+                // stepTurn(board, stepTurn);
+            }
 
-    });
-}
-
+        
+    
 
 
+})().then(() => { });
 
 
+// (async function main() {
+//     const board = await menu.userMove();
+//     console.log([board[0]])
+//     console.log([board[1]])
 
+// })().then(() => { });
+
+/// check Promise : 
+// save, load work 
+// is restart work 
+// user move work 
+// is load game work 
